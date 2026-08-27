5120fef3431694707163d76481ceea084bd0ae0c
Alejandro <asaavedram@cuc.edu.co>
2026-08-26 23:02:16 -0500

    asdadasd

--- Crud/app.js
+++ Crud/app.js
@@ -1,5 +1,4 @@
 // Aplicación para la ( agrego parte editada) gestión de usuarios y productos.
 //otro comentario para ver si se guarda
 //aaaaaaaaaaaaadadada
-// ya cree el conflicto solo falta tirar pull request para que quede en los registros de github(profe esto me llevo mas tiempo de lo que tenia pensado ya estoy cansado y me voy a dormir)
-//tiro esta segunda linea por si acaso
+// creando un conflicto para resolverlo posteriormente
