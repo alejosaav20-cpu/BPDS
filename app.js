ef488d6e3d7ea1a3fc1cd2f0dce5da4e36c369aa
SAAVEDRA MORELO ALEJANDRO <asaavedram@cuc.edu.co>
2026-08-27 16:56:49 -0500

    se cambio todo con next

--- app.js
+++
@@ -1,4 +0,0 @@
-// Aplicación para la ( agrego parte editada) gestión de usuarios y productos.
-//otro comentario para ver si se guarda
-//aaaaaaaaaaaaadadada
-// creando un conflicto para resolverlo posteriormente
--- new_version/system.py
+++
@@ -1,5 +0,0 @@
-print("aqui esta el sistemas mas mejor")
-sistema = "mas mejor" 
-+ str(input("es sistema es?"))
-if sistema == " el mas mejor":
-    print("si es el sistema mas mejor")
