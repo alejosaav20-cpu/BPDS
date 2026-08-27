ca84650010f660d1411bebdc23335d1db4781c85
Alejandro <asaavedram@cuc.edu.co>
2026-08-26 21:46:14 -0500

    creando un conflicto para resolverlo posteriormente

--- Crud/new_version/system.py
+++ Crud/new_version/system.py
@@ -1,3 +1,5 @@
 print("aqui esta el sistemas mas mejor")
 sistema = "mas mejor" 
-#hola como estan?
++ str(input("es sistema es?"))
+if sistema == " el mas mejor":
+    print("si es el sistema mas mejor")
