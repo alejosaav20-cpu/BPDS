64f7aafad736d2eeefa6db275936c21b7fe4436a
Alejandro <asaavedram@cuc.edu.co>
2026-08-26 22:11:28 -0500

    cambio en una linea

--- Crud/app.js
+++ Crud/app.js
@@ -1,4 +1,4 @@
 // Aplicación para la ( agrego parte editada) gestión de usuarios y productos.
-//otro comentario para ver si se guarda
-//aaaaaaaaaaaaadadada
-// creando un conflicto para resolverlo posteriormente
+//edite esto para que cambiara todo
+
+// recreando un conflicto a proposito
--- Crud/new_version/system.py
+++ Crud/new_version/system.py
@@ -1,5 +1,3 @@
 print("aqui esta el sistemas mas mejor")
 sistema = "mas mejor" 
-+ str(input("es sistema es?"))
-if sistema == " el mas mejor":
-    print("si es el sistema mas mejor")
+# creando un conflicto a proposito
